74b7262ddcc755294ec1516a33f41f84f7d4d47e
robertfloria <95577983+robertfloria@users.noreply.github.com>
2025-11-24 18:52:36 +0200

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   // Enable Global validation
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, //as
+      whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
     }),
