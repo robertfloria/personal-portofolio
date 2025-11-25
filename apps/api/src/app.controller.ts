46ffdb739ea5a6592a045b7effbdf247c416fa7d
Robert Nicolae Floria <95577983+robertfloria@users.noreply.github.com>
2025-11-25 13:13:28 +0200

    chore: remove unnecessary comment in app.controller.ts

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-//s
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
